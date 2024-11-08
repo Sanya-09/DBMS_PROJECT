77e181cff655f635d7aa13d8d1e95d01d7522e1b
Sanya-09 <agarwalsanya90@gmail.com>
2024-11-08 21:15:00 +0530

    Update admin_index.php

--- admin_index.php
+++ admin_index.php
@@ -1,128 +1,94 @@
-<!-- connect files -->
-<?php 
-include('includes/connect.php');
-include('functions/common_function.php');
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compartible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ecommerce Website</title>
-    <!-- Bootstrap css link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- font awesom  link-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Admin Dashboard</title>
+<!-- bootstrap css link -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <!-- css link-->
-     <link rel="stylesheet" href="style.css">
+<!-- font awesome link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- css file -->
+ <link rel="stylesheet" href="../style.css">
+
+ <style>
+.admin_image{
+    width: 100px;
+    object-fit: contain;
+}
+.footer{
+    position: absolute;
+    bottom:0;
+}
+
+ </style>
 </head>
 <body>
-    <!-- NAVIGATION BAR -->
-    <div class="container-fluid p-0">
+    <!-- nav bar-->
+     <div class="container-fluid p-0">
         <!-- first child -->
-        <nav class="navbar navbar-expand-lg bg-success">
-  <div class="container-fluid">
-    <img src="./img/Orange Simple Online Shopping Logo (1).png" alt="" class="logo">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-         </li>
-         <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
-         </li>
-         <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
-         </li>
-         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-         </li>
-         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
-         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Total Price:</a>
-         </li>
-        
-          <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-         <button class="btn btn-outline-light" type="submit">Search</button>
-         </form>
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../img/Orange Simple Online Shopping Logo (1).png" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg ">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link"> Welcome Guest</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <!-- second child -->
+         <div class="bg-light">
+            <h2 class="text-center">Manage Details </h2>
          </div>
-         </div>
-         </nav>
 
-    <!-- Second child -->
-     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
-        <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Welcome Guest</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
-        </ul>
-     </nav>
-
-     <!-- third child -->
-    <div class="bg-light">
-        <h3 class="text-center">Hidden Store</h3>
-        <p class="text-center">Communication is the heart of e-commerce community</p>
-    </div>
-
-
-         <!-- Fourth child -->
-<div class="row px-1">
-    <div class="col-md-10">
-        <!-- PRODUCTS -->
-        <div class="row">
-            <!-- Fetching products -->
-            <?php
-            // calling function 
-           getproducts();
-            ?>
-            <!-- Row end -->
+         <!-- third child -->
+<div class="row">
+    <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+        <div class="px-5">
+            <a href="#"><img src="../img/pineapple.jpeg" alt="Admin Profile" class="admin_image"></a>
+            <p class="text-light text-center">Admin Name</p>
         </div>
-        <!-- Column end -->
+        <!-- Improved button styling -->
+        <div class="button text-center">
+            <!-- Option 1: Use btn class from Bootstrap -->
+            <a href="insert_product.php" class="btn btn-info my-1">Insert Products</a>
+            <a href="view_products.php" class="btn btn-info my-1">View Products</a>
+            <a href="index.php?insert_category" class="btn btn-info my-1">Insert Categories</a>
+            <a href="view_categories.php" class="btn btn-info my-1">View Categories</a>
+            <a href="index.php?insert_brand" class="btn btn-info my-1">Insert Brands</a>
+            <a href="view_brands.php" class="btn btn-info my-1">View Brands</a>
+            <a href="all_orders.php" class="btn btn-info my-1">All orders</a>
+            <a href="all_payments.php" class="btn btn-info my-1">All payments</a>
+            <a href="list_users.php" class="btn btn-info my-1">List Users</a>
+            <a href="logout.php" class="btn btn-info my-1">Log out</a>
+        </div>
     </div>
-
-    <div class="col-md-2 bg-secondary p-0">
-    <ul class="navbar-nav me-auto text-center">
-       
-
-        <!-- Brands TO BE DISPLAYED -->
-        <li class="nav-item bg-info mt-3">
-            <a href="#" class="nav-link text-light"><h4>Delievery Brands</h4></a>
-        </li>
-
-        <?php
-        getbrands();
-        ?>
-
-         <!-- Categories TO BE DISPLAYED-->
-         <li class="nav-item bg-info">
-            <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
-        </li>
-
-        <?php
-        getcat();
-        ?>
-    </ul>
-</div>  
 </div>
-    <!-- last child -->
-    <div class="bg-info p-3 text-center"></div>
-    <p>All rights reserved- Designed by Sanya Agarwal </p>
+
+<!-- fourth child -->
+<div class="container">
+    <?php
+    // Corrected PHP syntax for checking GET parameter
+    if(isset($_GET['insert_category'])){
+        include('insert_categories.php');
+    }
+    if(isset($_GET['insert_brand'])){
+        include('insert_brands.php');
+    }
+    ?>
+</div>
+          <!-- last child -->
+    <div class="bg-info p-3 text-center footer"></div>
+    <p>All rights reserved-: Designed by Sanya Agarwal </p>
     </div>
-
- 
-    <!-- bootstrap js link -->
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+     </div>
+   <!-- bootstrap js link -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
